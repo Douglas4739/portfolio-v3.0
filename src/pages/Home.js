fcc78d19a490556c1a4f44bf320eeccb11cd0c6b
DouglasEde <briancarter340@gmail.com>
2024-02-21 16:13:47 -0800

    added 1 image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Nav } from '../components/Navbar';
 import './Home.css';
+import nd1 from '../images/nd1.jpg';
 
 export default function Home() {
   return (
     <>
     <div class="landing">
-    <div class="landingMessage">Home 1</div>
-    <div class='heroImage'>Home 2</div>
+    <div class="landingMessage">
+      <h2>Douglas Brian Ede</h2>
+      <h1>
+      Data-driven production manager 
+      skilled in 6 Sigma and web design</h1>
+    </div>
+    <div class='heroImage'>
+        <img src={nd1} alt='Auto Yard with Mountains in the background' style={{ width: '80%', height: 'auto' }} />
+    </div>
     </div>
     </>
   )
